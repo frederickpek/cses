71a60b0f5858f4adb1c4c6beced55f9d03e2201e
frederickpek <ssixthlhm@gmail.com>
2026-08-24 22:50:20 +0800

    Fix Minimizing Coins Python solution (1634)

--- problems/dynamic_programming/minimizing_coins_1634.py
+++ problems/dynamic_programming/minimizing_coins_1634.py
@@ -2,18 +2,20 @@
 let set of coin values be C
 dp[i] be minimum number of coins required to reach value i
 dp[i] = 1 if i in C else, min(dp[i - c] + 1 for c in C)
-
-python TLEs
 """
+
+inf = float("inf")
 
 def solve():
     n, T = map(int, input().split())
     C = list(map(int, input().split()))
-    dp = [0] * (T + 1)
-    for i in range(T + 1):
-        candidates = [dp[i - c] + 1 for c in C if (i - c >= 0)]
-        dp[i] = min(candidates) if candidates else 0
-    print(dp[T] or -1)
+    dp = [inf] * (T + 1)
+    dp[0] = 0
+    for c in C:
+        for i in range(T + 1):
+            if i - c < 0: continue
+            dp[i] = min(dp[i], dp[i - c] + 1)
+    print(-1 if dp[T] == inf else dp[T])
 
 if __name__ == "__main__":
     solve()
