444c4b4708c6da1e83c1bd20707c98f72a638184
Frederick Pek <ssixthlhm@gmail.com>
2026-08-24 14:47:41 +0800

    Update cheatsheet with heapq, fast I/O, and graph patterns

--- scripts/cheatsheet.py
+++ scripts/cheatsheet.py
@@ -3,12 +3,22 @@ from collections import deque
 from queue import PriorityQueue
 from bisect import bisect_left, bisect_right
 from functools import lru_cache, reduce
+from heapq import heappush, heappop
 from math import gcd, lcm, sqrt, sin, cos, tan, pi, e, inf
 
 import sys
 sys.setrecursionlimit(10**8)
 
-I = lambda: map(int, input().split())
+INF = float('inf')
+input = sys.stdin.readline
+output = sys.stdout.write
+output(" ".join(map(str, [1, 2, 3])) + "\n")
+
+n, m = map(int, input().split())
+G = [[] for _ in range(n)]
+for _ in range(m):
+    u, v, w = map(int, input().split())
+    G[u - 1].append((v - 1, w))
 
 def main():
     q = deque()
@@ -23,6 +33,9 @@ def main():
     pq.get()
     pq.empty()
 
+    pq = [(0, 0)]
+    d, u = heappop(pq)
+    heappush(pq, (d, v))
 
 if __name__ == "__main__":
     main()
