1dcf4ba7bfd263d136555cc39cedb9aeb1a0a932
frederickpek <ssixthlhm@gmail.com>
2026-08-25 02:38:49 +0800

    Solve Coin Combinations I (1635)

---
+++ problems/dynamic_programming/coin_combinations_i_1635.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, n) for (int i = 0; i < n; i++)
+
+const int MOD = 1e9 + 7;
+
+void solve() {
+	int n, T;
+	cin >> n >> T;
+	vector<int> C(n);
+	rep(i, n) cin >> C[i];
+
+    vector<int> dp(T + 1);
+    dp[0] = 1;
+
+    rep(i, T + 1) for (auto &c : C) {
+        if (i - c < 0) continue;
+        dp[i] = (dp[i] + dp[i - c]) % MOD;
+    }
+
+    cout << dp[T] << endl;
+}
+
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+    solve();
+}
